bb962e86539829833ad39f462866d846b642be31
Giovanni Alayan (RIT Student) <gna7400@rit.edu>
2022-01-31 13:47:11 -0500

    started server.js and htmlResponses.js

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+    console.log(request);
+  
+    switch (request.url) {
+        case '/':
+          htmlHandler.getIndex(request, response);
+          break;
+    
+        default:
+          htmlHandler.getIndex(request, response);
+          break;
+      }
+};
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`listening on 127.0.0.1:${port}`);
+});
