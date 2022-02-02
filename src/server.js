62c4373973bae492bcde78dcdb6c33e56a7f51f2
Giovanni Alayan (RIT Student) <gna7400@rit.edu>
2022-02-02 13:46:50 -0500

    added style.css handling

--- src/server.js
+++ src/server.js
@@ -1,20 +1,28 @@
 const http = require('http');
+const url = require('url');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getStyle,
+  notFound: htmlHandler.getIndex,
+};
+
 const onRequest = (request, response) => {
-    console.log(request);
+  console.log(request);
   
-    switch (request.url) {
-        case '/':
-          htmlHandler.getIndex(request, response);
-          break;
-    
-        default:
-          htmlHandler.getIndex(request, response);
-          break;
-      }
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+
+  if(urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  }
+  else {
+    urlStruct.notFound(request, response, params);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
